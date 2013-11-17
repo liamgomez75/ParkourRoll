39b1a478756a6f0980a4790bee549efb334a4075
James Healey <jameshealey1994@gmail.com>
2013-11-17 04:33:15 +0000

    Formatting and Javadoc. Also fixed incorrect "main" value in plugin.yml

--- src/com/gmail/liamgomez75/parkourroll/ParkourRoll.java
+++ src/com/gmail/liamgomez75/parkourroll/ParkourRoll.java
@@ -6,25 +6,35 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ParkourRoll extends JavaPlugin
-{
+/**
+ * Parkour Roll plugin for Bukkit.
+ *
+ * @author Liam Gomez
+ * @author JamesHealey94 <jameshealey1994.gmail.com>
+ */
+public class ParkourRoll extends JavaPlugin {
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         final DamageListener fallDamage = new DamageListener(this);
         getServer().getPluginManager().registerEvents(fallDamage, this);
         saveDefaultConfig();
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
-    {
-        if (cmd.getName().equalsIgnoreCase("Parkourroll"))
-        {
-            if (args.length > 0)
-            {
-                if ((args[0].equalsIgnoreCase("reload")))
-                {
+    /**
+     * Executes the command.
+     *
+     * @param sender            sender of the command
+     * @param cmd               command sent
+     * @param commandLabel      exact command string sent
+     * @param args              arguments given with the command
+     * @return                  if the command was executed correctly
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("Parkourroll")) {
+            if (args.length > 0) {
+                if ((args[0].equalsIgnoreCase("reload"))) {
                     return reload(sender);
                 }
             }
@@ -32,18 +42,21 @@ public class ParkourRoll extends JavaPlugin
         return false;
     }
 
-    public boolean reload(CommandSender sender)
-    {
-        if (sender.hasPermission("pkr.admin"))
-        {
+    /**
+     * Reloads the configuration.
+     *
+     * @param sender    sender of the command
+     * @return          if the sender has the correct permissions and reloaded
+     *                  the configuration correctly
+     */
+    public boolean reload(CommandSender sender) {
+        if (sender.hasPermission("pkr.admin")) {
             reloadConfig();
             sender.sendMessage(ChatColor.LIGHT_PURPLE + "Configuration reloaded.");
             return true;
+        } else {
+            sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
         }
-        else
-        {
-         sender.sendMessage(ChatColor.RED + "You don't have permission to do that!");
-        }    
         return false;
     }
 }
